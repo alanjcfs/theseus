1d4748dd7ea22d82a55a20a5c6ec38d08fce7a38
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2015-02-22 22:17:02 -0800

    Moar tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! The `theseus` crate provides a function that adds two to a number.
+//!
+//! # Examples
+//! ```
+//! assert_eq!(7, theseus::add_two(5));
+//! ```
+
+/// This function adds two to its argument.
+/// # Examples
+/// ```
+/// use theseus::add_two;
+/// assert_eq!(4, add_two(2));
+/// ```
 pub fn add_two(a: i32) -> i32 {
     a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(10, add_two(8));
+    }
 }
